237da9adb9ef197f112d0f94866ff6dac092f5ec
bandakkalokka <chathula.adikary@gmail.com>
2017-03-11 20:16:34 -0700

    Implemented the StartDriving() from UnitTest

--- PROJECT_4/Driving_Functions.ino
+++ PROJECT_4/Driving_Functions.ino
@@ -1,7 +1,26 @@
+#define MAX_RIGHT_SPEED             1400                                        // Ideal Right Servo Max Speed to got straight
+#define MAX_LEFT_SPEED              1638                                        // Ideal Left Servo Max Speed to got straight
+#define MIN_SPEED                   1500                                        // Ideal Stop Servo Speed 
+#define MAX_DELTA_SPEED             (MAX_LEFT_SPEED - MAX_RIGHT_SPEED) / 2      // Approximate Max Speed
+
+int ServoRightSpeed = MIN_SPEED;
+int ServoLeftSpeed = MIN_SPEED;
+
 void StartDriving(){
-  servoRight.writeMicroseconds(1700);
-  servoLeft.writeMicroseconds(1310);}
+  
+  for(int deltaSpeed = 0; deltaSpeed < MAX_DELTA_SPEED; deltaSpeed++) {
+    ServoLeftSpeed = MIN_SPEED + deltaSpeed;
+    ServoRightSpeed = MIN_SPEED - deltaSpeed;
+    
+    ServoLeft.writeMicroseconds(ServoLeftSpeed);
+    ServoRight.writeMicroseconds(ServoRightSpeed);
+    delay(10);                                                                  // Apply the deltaSpeed for 10ms
+  }
+  
+  ServoLeft.writeMicroseconds(MAX_LEFT_SPEED);                                  // Set Servo's to maximum speed 
+  ServoRight.writeMicroseconds(MAX_RIGHT_SPEED);                                // after acceleration is completed
+}
 
 void StopDriving(){
-    servoRight.writeMicroseconds(1500);
-    servoLeft.writeMicroseconds(1500); }
+    ServoRight.writeMicroseconds(1500);
+    ServoLeft.writeMicroseconds(1500); }
