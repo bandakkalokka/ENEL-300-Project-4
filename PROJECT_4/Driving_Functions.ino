4b78d1b14cd50780e0c66aff84d06b8f6018be13
bandakkalokka <chathula.adikary@gmail.com>
2017-03-12 20:23:49 -0600

    Implemented the StopDriving() from UnitTests

--- PROJECT_4/Driving_Functions.ino
+++ PROJECT_4/Driving_Functions.ino
@@ -1,26 +1,38 @@
-#define MAX_RIGHT_SPEED             1400                                        // Ideal Right Servo Max Speed to got straight
-#define MAX_LEFT_SPEED              1638                                        // Ideal Left Servo Max Speed to got straight
-#define MIN_SPEED                   1500                                        // Ideal Stop Servo Speed 
-#define MAX_DELTA_SPEED             (MAX_LEFT_SPEED - MAX_RIGHT_SPEED) / 2      // Approximate Max Speed
+#define MAX_RIGHT_SPEED             1400                                          // Ideal Right Servo Max Speed to got straight
+#define MAX_LEFT_SPEED              1638                                          // Ideal Left Servo Max Speed to got straight
+#define MIN_SPEED                   1500                                          // Ideal Stop Servo Speed 
+#define MAX_DELTA_SPEED             (MAX_LEFT_SPEED - MAX_RIGHT_SPEED) / 2        // Approximate Max Speed
+#define MIN_DELTA_SPEED               0
+#define ACCELERATION                  1
 
 int ServoRightSpeed = MIN_SPEED;
 int ServoLeftSpeed = MIN_SPEED;
 
 void StartDriving(){
   
-  for(int deltaSpeed = 0; deltaSpeed < MAX_DELTA_SPEED; deltaSpeed++) {
+  for(int deltaSpeed = 0; deltaSpeed < MAX_DELTA_SPEED; deltaSpeed += ACCELERATION) {
     ServoLeftSpeed = MIN_SPEED + deltaSpeed;
     ServoRightSpeed = MIN_SPEED - deltaSpeed;
     
     ServoLeft.writeMicroseconds(ServoLeftSpeed);
     ServoRight.writeMicroseconds(ServoRightSpeed);
-    delay(10);                                                                  // Apply the deltaSpeed for 10ms
+    delay(10);                                                                    // Apply the deltaSpeed for 10ms
   }
   
-  ServoLeft.writeMicroseconds(MAX_LEFT_SPEED);                                  // Set Servo's to maximum speed 
-  ServoRight.writeMicroseconds(MAX_RIGHT_SPEED);                                // after acceleration is completed
+  ServoLeft.writeMicroseconds(MAX_LEFT_SPEED);                                    // Set Servo's to maximum speed 
+  ServoRight.writeMicroseconds(MAX_RIGHT_SPEED);                                  // after acceleration is completed
 }
 
 void StopDriving(){
-    ServoRight.writeMicroseconds(1500);
-    ServoLeft.writeMicroseconds(1500); }
+  for(int deltaSpeed = MAX_DELTA_SPEED; deltaSpeed >= MIN_DELTA_SPEED; deltaSpeed -= ACCELERATION) {
+    ServoLeftSpeed = MIN_SPEED + deltaSpeed;
+    ServoRightSpeed = MIN_SPEED - deltaSpeed;
+    
+    ServoLeft.writeMicroseconds(ServoLeftSpeed);
+    ServoRight.writeMicroseconds(ServoRightSpeed);
+    delay(10);                                                                    // Apply the deltaSpeed for 10ms
+  }
+
+  ServoLeft.writeMicroseconds(MIN_SPEED);                                         // Set Servo's to stop after
+  ServoRight.writeMicroseconds(MIN_SPEED);                                        // deceleration is complete
+}
