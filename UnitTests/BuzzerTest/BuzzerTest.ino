a2023c01484433e5dbe44e2533fb6273661dde1f
bandakkalokka <chathula.adikary@gmail.com>
2017-03-29 17:12:28 -0600

    Finalized Buzzer/LED UnitTest

--- UnitTests/BuzzerTest/BuzzerTest.ino
+++ UnitTests/BuzzerTest/BuzzerTest.ino
@@ -3,11 +3,9 @@
  * void Beep()
  * 
  * The purpose of the Beep function is to inform that the patient has been found.
- * Originally, both the buzzer and LED were attached to pin 10 along with a ____ resistor.
+ * Originally, both the buzzer and LED were attached to pin 10 along with a 1k resistor.
  * However, soon it was evident that the current required to drive the pin was too high.
  * Then the decision was taken to use pin 10 for Buzzer and pin 7 for LED.
- * A ---- resistor was used for the buzzer and a ----- resistor was used for the LED
- * as indicated in the Schematic
  * 
  */
 
@@ -21,17 +19,22 @@ void setup() {
   /* Assign pinModes to Buzzer and LED */
   pinMode(BUZZER, OUTPUT);
   pinMode(LED, OUTPUT);
+  Beep();                                              // Call to Beep function
 
-  /* Beep the Buzzer 5 times */
+}
+
+
+void Beep() {
+  // Beep Buzzer 5 times
   for(int i = 0; i < 5; i++){
     tone(BUZZER, 1000, 500);
     delay(1000);
   }
 
-  /* Blink LED once */
+  //Blink LED once
   digitalWrite(LED, HIGH);
   delay(500);
-  digitalWrite(LED,LOW);
+  digitalWrite(LED,LOW); 
 }
 
 void loop() {
